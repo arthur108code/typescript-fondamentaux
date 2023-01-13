d96d9b2213936543a60ff0788c4b2b391f892cc2
Arthur108 <arthur7joly@gmail.com>
2023-01-13 23:27:04 +0100

    exo 2 OK

--- src/exercise/02.ts
+++ src/exercise/02.ts
@@ -10,19 +10,25 @@ init()
 
 // 🐶 Créer une variable `names` mais sans initiliser de valeurs
 //  ajoute ensuite 'John' et 'doe' à `name` sans utiliser la méthode `push()`
-let names = []
+let names: string[]
+names = ['John','Doe']
 
 // 🤖 affiche 'Il y a 2 noms' dans le navigateur grace à `displayText` et `.length`
-
+displayText(`Il y a ${names.length} noms`)
 // 🐶 Créer une constante `games` et initialise avec un tableau vide
 // avec la méthode `push()` ajoute 3 jeux
-// ⛏️ const games
+const games :string[] = []
+games.push('jeux1','jeux2','jeux3')
+console.log(games);
 // 🤖 affiche le nombre d'élément à l'ecran
+displayText(`Il y a ${games.length} jeux`)
 
 // 🐶 Créer une constante `things` : un tableau qui peut prendre n'importe quel type de donnée
-// ⛏️ const things
-
+const things: any[] = []
+things.push(1,'2',3,true)
 // 🤖 affiche le nombre d'élément à l'ecran
+console.log(things);
+displayText(`Il y a ${things.length} choses`)
 
 /*
 eslint
