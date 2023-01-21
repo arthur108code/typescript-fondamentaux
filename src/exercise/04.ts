35ffeaadbc878ae2a838a9c548fa9fdd14d8a14a
ajoly <arthur.joly@zoomalia.com>
2023-01-21 14:14:58 +0100

    Exo 4 OK

--- src/exercise/04.ts
+++ src/exercise/04.ts
@@ -4,6 +4,7 @@
 // ❌ NE PAS MODIFIER
 // Utilitaire ne faisant pas partie de l'exercice
 import displayText, {init} from './helper/exerciseHelper'
+
 init()
 
 // ✔️ Début de l'exercice
@@ -11,24 +12,26 @@ init()
 // 🐶 Déclare un type `pinType` qui permet d'avoir un number ou string
 // affecte ce type à la variable `pin`
 
-let pin: number | string // ⛏️ remplace cette declaration
+type Pin = number | string // ⛏️ remplace cette declaration
+let pin: Pin
 pin = 0
 displayText(`Le pin est ${pin}`)
 pin = '10'
 displayText(`Le pin est ${pin}`)
 
 // 🐶 Déclare un type `primitives` qui permet d'avoir tous les types primitif number | boolean | string
-
+type Primitives = number | boolean | string
 // ⛏️ remplace `any` par le type `primitives` et affecte une bonne valeur
-let prim: any = {}
+let prim: Primitives
+prim = 'Mike'
 displayText(`prim vaut  ${prim}`)
 
 // 🐶 modifie le type `primitivesNullUndefined` pour que les lignes suivantes compilent
-type primitivesNullUndefined = number | boolean | string
+type primitivesNullUndefined = number | boolean | string | null | undefined
 
 // ⛏️ décommente le code ci-dessous et fait en sorte que la compilation fonctionne en modifiant `primitivesNullUndefined`
-// let prim2: primitivesNullUndefined
-// displayText(`prim2 vaut  ${prim2}`)
+let prim2: primitivesNullUndefined
+displayText(`prim2 vaut  ${prim2}`)
 
 // 🐶 Modifie le type `Person` avec les propiétés
 // - name de type string
@@ -38,23 +41,34 @@ type primitivesNullUndefined = number | boolean | string
 // - params un object non defini
 // - payload peut avoir n'importe quel type
 
-type Person = any
+type Person = {
+    name: string
+    age: number
+    isActive: boolean
+    roles: string []
+    params: object
+    payload: any
+    friend: Person | undefined
+}
 
 const person: Person = {
-  name: 'John',
-  age: 23,
-  isActive: true,
-  roles: ['admin'],
-  params: {id: '50'},
-  payload: undefined,
+    name: 'John',
+    age: 23,
+    isActive: true,
+    roles: ['admin'],
+    params: {id: '50'},
+    payload: undefined,
+    friend: undefined
+
 }
 
 displayText(
-  `${person.name} a ${person.age} ans et a le role ${person.roles[0]} `,
+    `${person.name} a ${person.age} ans et a le role ${person.roles[0]} `,
 )
 
 // 🐶 N'oublie pas l'exercice bonus
 
+displayText(`${person.name} a pour ami : ${person.friend?.name ?? 'aucun'}`)
 /*
 eslint
   @typescript-eslint/no-unused-vars: "off"
